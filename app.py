059db41d5b395e3e8c139b844405210ec074703c
Ripan Debnath <111954951+debnath94@users.noreply.github.com>
2023-06-23 15:22:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,32 +15,35 @@ model = load_model("your_model.h9")
 
 def main():
     st.title("Brain Tumor Classification")
-    st.text("Upload an MRI scan image for tumor classification")
+    st.text("Upload MRI scan images for tumor classification")
 
     # File upload
-    uploaded_file = st.file_uploader("Choose an MRI scan image or video", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+    uploaded_files = st.file_uploader("Choose MRI scan images", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
-    if uploaded_file is not None:
-        # Display the uploaded image
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+    if uploaded_files is not None:
+        for uploaded_file in uploaded_files:
+            # Display the uploaded image
+            image = Image.open(uploaded_file)
+            st.image(image, caption="Uploaded Image", use_column_width=True)
 
-        # Preprocess the image
-        image = image.resize((150, 150))
-        image = np.array(image) / 255.0
-        image = np.expand_dims(image, axis=0)
+            # Preprocess the image
+            image = image.resize((150, 150))
+            image = np.array(image) / 255.0
+            image = np.expand_dims(image, axis=0)
 
-        # Make predictions
-        prediction = model.predict(image)
-        if prediction[0][0] > 0.5:
-            result = "Tumor"
-        else:
-            result = "Normal"
+            # Make predictions
+            prediction = model.predict(image)
+            if prediction[0][0] > 0.5:
+                result = "Tumor"
+            else:
+                result = "Normal"
 
-        st.write("Prediction:", result)
+            st.write("Prediction:", result)
+            st.write("---")
 
 if __name__ == "__main__":
     main()
+
 
 
 
